21ec70c80b06a7f9bc4d43a1a120af6ec97d1878
Inzamam-Ul Haque <132208469+InzamamulHaque0786@users.noreply.github.com>
2024-09-16 20:43:17 +0530

    Login page completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,55 +1,57 @@
-import { Button, FormControl, FormControlLabel, FormLabel, Grid2, Link, Paper, Radio, RadioGroup, TextField,Typography } from '@mui/material';
+import { Avatar, Button, FormControl, Grid2, InputLabel, Link, MenuItem, Paper, Select, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react'
+import { Lock } from '@mui/icons-material';
 
 const Login = () => {
-    const [inputs,setInputs]=useState({
-        name: '',
-        email: '',
-        password: "",
-        role:'',
-    });
-    // input change function 
-    const handleChange=(e)=>{
-        setInputs(prevState=>({
-            ...prevState,
-            [e.target.name]:[e.target.value],
-          }))
-    }
-    //form handling
-      const handleSubmit=(e)=>{
-        e.preventDefault();
-        console.log(inputs);
-   }
-   //paper style
-   const paperStyle={padding:20,height:'80vh',width:280,margin:'20px auto'}
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: "",
+    role: '',
+  });
+  // input change function 
+  const handleChange = (e) => {
+    setInputs(prevState => ({
+      ...prevState,
+      [e.target.name]: [e.target.value],
+    }))
+  }
+  //form handling
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  }
+  //paper style
+  const paperStyle = { padding: '40px 30px', width: 250, margin: '40px auto' }
   return (
     <Grid2>
-    <Paper  elevation={10} style={paperStyle}>
-            
-    <form  onSubmit={handleSubmit}>
-        <TextField fullWidth label='Name' name='name' type='text' value={inputs.name} onChange={handleChange} variant='standard' placeholder='Enter Name'>hello</TextField>
-        {/* <TextField fullWidth name='name' type='text' value={inputs.name} onChange={handleChange} varient='standard' placeholder='Enter your Name'></TextField> */}
-        <br />
-         <TextField fullWidth variant='standard' label='Email' name='email' type='email' value={inputs.email} onChange={handleChange}  placeholder='Enter Email'></TextField>
-        <br />
-         <TextField fullWidth variant='standard' label='Password' name='password' type='password' value={inputs.password} onChange={handleChange} varient="outlined" placeholder='Enter Password'></TextField>
-        <br />
-        <FormControl>
-                  <FormLabel>Role</FormLabel>
-                  <RadioGroup name='role' defaultValue={'Admin'} onChange={handleChange}>
-                     <FormControlLabel label='Admin' value={'Admin'} control={<Radio/>}/>
-                     <FormControlLabel label='Employee' value={'Employee'} control={<Radio/>}/>
-                     <FormControlLabel label='Customer' value={'Customer'} control={<Radio/>}/>
-                  </RadioGroup>
-        </FormControl>
-        
-        
-
-        <Button fullWidth variant='contained'  type="submit" sx={{margin:'8px 0'}}>Sign in</Button>
-        <Typography>Do you have an account? <Link href="#" underline="none">Sign Up</Link>
-        </Typography>
-    </form>
-    </Paper>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid2 align='center' sx={{ paddingBottom: "35px" }}>
+          <Avatar sx={{ bgcolor: '#1976d2' }}><Lock /></Avatar>
+          <Typography variant="h3">Login</Typography>
+        </Grid2>
+        <form onSubmit={handleSubmit}>
+          <TextField size='small' fullWidth variant='outlined' name='email' type='email' value={inputs.email} onChange={handleChange} placeholder='Enter Email'></TextField>
+          <br />
+          <br />
+          <TextField size='small' fullWidth variant='outlined' name='password' type='password' value={inputs.password} onChange={handleChange} varient="outlined" placeholder='Enter Password'></TextField>
+          <br />
+          <br />
+          <FormControl size='small' fullWidth >
+            <InputLabel id="menu">Role</InputLabel>
+            <Select labelId='menu' id='menu-list' label="role" name="role" value={inputs.role} onChange={handleChange}>
+              <MenuItem value={'Admin'}>Admin</MenuItem>
+              <MenuItem value={'Employee'}>Employee</MenuItem>
+              <MenuItem value={'Customer'}>Customer</MenuItem>
+            </Select>
+          </FormControl>
+          <br />
+          <br />
+          <Button fullWidth variant='contained' type="submit" sx={{ margin: '15px 0' }}>LOGIN</Button>
+          <br />
+          <Typography variant="caption" sx={{paddingLeft:'30px'}} >Do you have an account? <Link href="#" underline="none">Sign Up</Link>
+          </Typography>
+        </form>
+      </Paper>
     </Grid2>
   )
 }
