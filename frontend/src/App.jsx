572d6a4dfc3a6709ea9a1dcf65315c0e62fa5b27
Inzamam-Ul Haque <132208469+InzamamulHaque0786@users.noreply.github.com>
2024-09-16 21:34:09 +0530

    created some pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Admin from './pages/Admin'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Login/>
+      {/* <Login/> */}
+      {/* <Signup/> */}
+      <Navbar/>
+      <Admin/>
     </div>
   )
 }
