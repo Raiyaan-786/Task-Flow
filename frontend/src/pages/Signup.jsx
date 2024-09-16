4b0a52efa6b25d4a7d8147453f657175b13f0efd
Inzamam-Ul Haque <132208469+InzamamulHaque0786@users.noreply.github.com>
2024-09-16 21:04:41 +0530

    Signup completed

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,63 @@
+import { Avatar, Button, FormControl, Grid2, InputLabel, Link, MenuItem, Paper, Select, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react'
+import { Lock } from '@mui/icons-material';
+
+const Signup = () => {
+  const [inputs, setInputs] = useState({
+    name:'dfdf',
+    email: '',
+    password: "",
+    role: '',
+  });
+  // input change function 
+  const handleChange = (e) => {
+    setInputs(prevState => ({
+      ...prevState,
+      [e.target.name]: [e.target.value],
+    }))
+  }
+  //form handling
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  }
+  //paper style
+  const paperStyle = { padding: '25px 30px', width: 250, margin: '35px auto' }
+  return (
+    <Grid2>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid2 align='center' sx={{ paddingBottom: "20px" }}>
+          <Avatar sx={{ bgcolor: '#1976d2' }}><Lock /></Avatar>
+          <Typography variant="h3">Signup</Typography>
+        </Grid2>
+        <form onSubmit={handleSubmit}>
+          <TextField size='small' fullWidth variant='outlined' name='name' type='text' value={inputs.name} onChange={handleChange} placeholder='Name'></TextField>
+          <br />
+          <br />
+          <TextField size='small' fullWidth variant='outlined' name='email' type='email' value={inputs.email} onChange={handleChange} placeholder='Email'></TextField>
+          <br />
+          <br />
+          <TextField size='small' fullWidth variant='outlined' name='password' type='password' value={inputs.password} onChange={handleChange} varient="outlined" placeholder='Password'></TextField>
+          <br />
+          <br />
+          <FormControl size='small' fullWidth >
+            <InputLabel id="menu">Role</InputLabel>
+            <Select labelId='menu' id='menu-list' label="role" name="role" value={inputs.role} onChange={handleChange}>
+              <MenuItem value={'Admin'}>Admin</MenuItem>
+              <MenuItem value={'Employee'}>Employee</MenuItem>
+              <MenuItem value={'Customer'}>Customer</MenuItem>
+            </Select>
+          </FormControl>
+          <br />
+          <br />
+          <Button fullWidth variant='contained' type="submit" sx={{ margin: '15px 0' }}>LOGIN</Button>
+          <br />
+          <Typography variant="caption" sx={{paddingLeft:'30px'}} >Already have an account? <Link href="#" underline="none">Login</Link>
+          </Typography>
+        </form>
+      </Paper>
+    </Grid2>
+  )
+}
+
+export default Signup
