b08111fa9ea38cd66dbee4b272b53cd43f8f013b
Inzamam-Ul Haque <132208469+InzamamulHaque0786@users.noreply.github.com>
2024-10-12 15:58:40 +0530

    login and signup working as intended

--- frontend/src/pages/authentication/Login.jsx
+++ frontend/src/pages/authentication/Login.jsx
@@ -5,34 +5,18 @@ import API from '../../api/api';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const [inputs, setInputs] = useState({
-    email:'',
-    password:''
-  });
-  // input change function 
-  const handleChange = (e) => {
-    setInputs(prevState => ({
-      ...prevState,
-      [e.target.name]: [e.target.value],
-    }))
-  }
-  //form handling
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(inputs);
-  }
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
   //paper style
   const paperStyle = {padding: '40px 30px', width: 350, margin: '50px auto' }
-  
-  // Login API 
-  // Isme email aur password ke jagah apna variable jo use kiye ho wo daal dena 
+  // Login API  
   const navigate = useNavigate();
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const { data } = await API.post('/auth/login', {
-        email: 'inzamam1@gmail.com',
-        password: '1234'
+        email: email,
+        password: password
       });
       localStorage.setItem('token', data.token);  
       navigate('/');
@@ -49,12 +33,11 @@ const Login = () => {
           <Avatar sx={{ bgcolor: '#1976d2' }}><Lock /></Avatar>
           <Typography variant="h3">Login</Typography>
         </Grid2>
-        {/* <form onSubmit={handleSubmit}> */}
         <form onSubmit={handleLogin}>
-          <TextField size='small' fullWidth variant='outlined' name='email' type='email' value={inputs.email} onChange={handleChange} placeholder='Email'></TextField>
+          <TextField size='small' fullWidth variant='outlined' name='email' type='email' value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='Email'></TextField>
           <br />
           <br />
-          <TextField size='small' fullWidth variant='outlined' name='password' type='password' value={inputs.password} onChange={handleChange} varient="outlined" placeholder='Password'></TextField>
+          <TextField size='small' fullWidth variant='outlined' name='password' type='password' value={password} onChange={(e)=>{setPassword(e.target.value)}} varient="outlined" placeholder='Password'></TextField>
           <br />
           <br />
           <Button fullWidth variant='contained' type="submit" sx={{ margin: '15px 0' }}>LOGIN</Button>
--- frontend/src/pages/authentication/Signup.jsx
+++ frontend/src/pages/authentication/Signup.jsx
@@ -8,40 +8,23 @@ import { useNavigate } from 'react-router-dom';
 const Signup = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode)
-  const [inputs, setInputs] = useState({
-    name: '',
-    email: '',
-    password: '',
-    // phone:"",
-  });
-  // input change function 
-  const handleChange = (e) => {
-    setInputs(prevState => ({
-      ...prevState,
-      [e.target.name]: [e.target.value],
-    }))
-  }
 
-  //form handling
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(inputs);
-  // }
+  const [name,setName]=useState("");
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
 
   //paper style
   const paperStyle = { padding: '40px 30px', width: 350, margin: '50px auto' }
 
   // Sign up APi 
-  // Add phone number field also as it is only for customers
-  // Yahan pr name , email , phone , password ke jagah apna variable daal dena 
   const navigate = useNavigate()
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
       const response = await API.post('/auth/register',{
-        name: 'Inazmam',
-        email: 'inzamam1@gmail.com',
-        password: '1234'
+        name:name,
+        email:email,
+        password:password
       },{
         headers: {
           'Content-Type': 'application/json',
@@ -63,16 +46,13 @@ const Signup = () => {
             <Typography variant="h3">Signup</Typography>
           </Grid2>
           <form onSubmit= {handleSignUp}>
-            <TextField size='small' fullWidth variant='outlined' name='name' type='text' value={inputs.name} onChange={handleChange} placeholder='Name'></TextField>
+            <TextField size='small' fullWidth variant='outlined' name='name' type='text' value={name} onChange={(e)=>{setName(e.target.value)}} placeholder='Name'></TextField>
             <br />
             <br />
-            <TextField size='small' fullWidth variant='outlined' name='email' type='email' value={inputs.email} onChange={handleChange} placeholder='Email'></TextField>
+            <TextField size='small' fullWidth variant='outlined' name='email' type='email' value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='Email'></TextField>
             <br />
             <br />
-            {/* <TextField size='small' fullWidth variant='outlined' name='phone' type='number' value={inputs.phone} onChange={handleChange} placeholder='Phone'></TextField>
-            <br />
-            <br /> */}
-            <TextField size='small' fullWidth variant='outlined' name='password' type='password' value={inputs.password} onChange={handleChange} varient="outlined" placeholder='Password'></TextField>
+            <TextField size='small' fullWidth variant='outlined' name='password' type='password' value={password} onChange={(e)=>{setPassword(e.target.value)}} varient="outlined" placeholder='Password'></TextField>
             <br />
             <br />
             <Button fullWidth variant='contained' type="submit" sx={{ margin: '15px 0' }}>SIGNUP</Button>
