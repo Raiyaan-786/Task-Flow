cea1ffe47c1c6be63eb4d6c930238365a470a2b7
Inzamam-Ul Haque <132208469+InzamamulHaque0786@users.noreply.github.com>
2025-01-04 18:28:08 +0530

    edit work page some changes done

--- frontend/src/pages/workmanager/EditWorkPage.jsx
+++ frontend/src/pages/workmanager/EditWorkPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { TextField, Button, Box, Typography } from '@mui/material';
+import { TextField, Button, Box, Grid2 } from '@mui/material';
 import API from '../../api/api';
 import { useTheme } from '@emotion/react';
 import { tokens } from '../../theme';
@@ -9,18 +9,17 @@ import Header from '../../components/Header';
 const EditWorkPage = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-
   const { id } = useParams(); // Get the work ID from the URL
   const navigate = useNavigate();
   const [work, setWork] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
-  
+
   useEffect(() => {
     const fetchWorkDetails = async () => {
       try {
         const response = await API.get(`/getwork/${id}`);
-        setWork(response.data);
+        setWork(response.data.work); // Assuming response.data contains the work object
         setLoading(false);
       } catch (err) {
         setError('Failed to fetch work details');
@@ -33,7 +32,10 @@ const EditWorkPage = () => {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setWork({ ...work, [name]: value });
+    setWork((prevWork) => ({
+      ...prevWork,
+      [name]: value, // Dynamically updating the field in the work object
+    }));
   };
 
   const handleSaveChanges = async () => {
@@ -54,8 +56,8 @@ const EditWorkPage = () => {
   if (error) return <p>{error}</p>;
 
   return (
-    <Box display={'flex'} flexDirection={'column'} height={'88%'} margin={'10px'} p={.1}>
-      <Header title={'Work Profile'}/>
+    <Box display={'flex'} flexDirection={'column'} height={'88%'} overflow={'auto'} margin={'10px'} p={0.1}>
+      <Header title={'Work Profile'} />
       <Box
         bgcolor={colors.primary[900]}
         flexGrow={1}
@@ -64,40 +66,228 @@ const EditWorkPage = () => {
         flexDirection="column"
         borderRadius={'10px'}
       >
-      <TextField
-        label="Billing Name"
-        name="billingName"
-        value={work?.billingName || ''}
-        onChange={handleInputChange}
-        fullWidth
-        sx={{ mb: 2 }}
-      />
-      <TextField
-        label="Service"
-        name="service"
-        value={work?.service || ''}
-        onChange={handleInputChange}
-        fullWidth
-        sx={{ mb: 2 }}
-      />
-      <TextField
-        label="Work Type"
-        name="workType"
-        value={work?.workType || ''}
-        onChange={handleInputChange}
-        fullWidth
-        sx={{ mb: 2 }}
-      />
-      {/* Add more fields here */}
+        {work && (
+          <Grid2 container spacing={2} gap={2} padding={"10px 20px"} color={colors.grey[200]}>
+            {/* Customer */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>CUSTOMER</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.customer || ''} // Default to empty string if undefined
+                variant="filled"
+                onChange={handleInputChange}
+                name="customer" // Field name for dynamic update
+                fullWidth
+              />
+            </Grid2>
 
-      <Box display="flex" justifyContent="flex-end">
-        <Button onClick={() => navigate('/')} sx={{ mr: 1 }}>
-          Cancel
-        </Button>
-        <Button variant="contained" onClick={handleSaveChanges}>
-          Save Changes
-        </Button>
-      </Box>
+            {/* Billing Name */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>BILLING NAME</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.billingName || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="billingName"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Email */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>EMAIL</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.email || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="email"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Mobile */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>MOBILE</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.mobile || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="mobile"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* PAN */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>PAN</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.pan || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="pan"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Address */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>ADDRESS</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.address || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="address"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Service */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>SERVICE</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.service || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="service"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Work Type */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>WORK TYPE</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.workType || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="workType"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Assigned Employee */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>ASSIGNED EMPLOYEE</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.assignedEmployee || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="assignedEmployee"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Month */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>MONTH</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.month || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="month"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Quarter */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>QUARTER</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.quarter || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="quarter"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Financial Year */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>FINANCIAL YEAR</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.financialYear || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="financialYear"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Price */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>PRICE</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.price || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="price"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Quantity */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>QUANTITY</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.quantity || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="quantity"
+                fullWidth
+              />
+            </Grid2>
+
+            {/* Discount */}
+            <Grid2 size={6} display={'flex'} alignItems={'center'}>
+              <label>DISCOUNT</label>
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                value={work.discount || ''}
+                variant="filled"
+                onChange={handleInputChange}
+                name="discount"
+                fullWidth
+              />
+            </Grid2>
+
+            <Box display="flex" justifyContent="flex-end" width="100%" mt={2}>
+              <Button onClick={() => navigate('/')} sx={{ mr: 1 }}>
+                Cancel
+              </Button>
+              <Button variant="contained" onClick={handleSaveChanges}>
+                Save Changes
+              </Button>
+            </Box>
+          </Grid2>
+        )}
       </Box>
     </Box>
   );
